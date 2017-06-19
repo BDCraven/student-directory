cec1266ae05c56ab1a37487758215a607cb3be7a
BDCraven <bencraven@hotmail.com>
2017-06-19 14:36:28 +0100

    Modify to only print students whose name is shorter than 12 characters

--- directory.rb
+++ directory.rb
@@ -23,22 +23,10 @@ def print_header
 end
 
 def print(students)
-  puts "Please enter the first letter of the name of the students you wish to print"
-  first_name_let = gets.chomp
-  results = 0
-  if first_name_let == ""
-    puts "No letter entered."
-    exit
-  elsif
-    students.each_with_index do |student, student_number|
-      if student[:name][0].downcase.include?(first_name_let.downcase)
+  students.each_with_index do |student, student_number|
+      if student[:name].length <= 12
         puts "#{student_number + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-        results += 1
       end
-    end
-  end
-  if results == 0
-      puts "We don't have any students starting with #{first_name_let}"
   end
 end
 
