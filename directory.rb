73586d9a0f883bf6c2852e80d830fb5665b33f88
BDCraven <bencraven@hotmail.com>
2017-06-19 12:25:04 +0100

    Add student number

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, student_number|
+    puts "#{student_number + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
