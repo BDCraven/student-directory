1d5ca465d44454895fa5f27185d2a96a7776e5d8
BDCraven <bencraven@hotmail.com>
2017-06-20 16:00:17 +0100

    Reset directory file to default at step 7 ready for step 9 exercises

--- directory.rb
+++ directory.rb
@@ -1,64 +1,39 @@
 def input_students
-  puts "Please enter the names and cohorts of the students"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  cohort_months = ["January", "February", "March", "April",
-             "May", "June", "July", "August",
-             "September", "October", "November", "December"]
   # get the first name
+  name = gets.chomp
   # while the name is not empty, repeat this code
-  while true do
-    puts "Please enter the student's full name"
-    name = gets.delete!("\n") # delete last return character. An alt to .chomp
-    break if name.empty?
-
-    puts "Please enter the month of the student's cohort or leave blank if you are not sure"
-    cohort = gets.gsub("\n","").capitalize # substitute newline for empty
-    # string. Another alternative to chomp.
-
-    while !cohort_months.include?(cohort) && cohort != ""
-      puts "That is not a recognised month please enter a month or leave blank if you are not sure"
-      cohort = gets.chomp.capitalize
-    end
-
-    if cohort == ""
-      cohort = "TBC"
-    end
+  while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} " + (students.count > 1 ? "students" : "student")
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # get another name from the user
+    name = gets.chomp
   end
   # return the array of students
-  exit if students.empty?
   students
 end
 
-@line_width = 55
 def print_header
-  puts "The students of Villains Academy".center(@line_width)
-  puts "-------------".center(@line_width)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-def print(students, cohort = "") # if no cohort argument then default is empty.
-  if cohort != "" # if not empty we select all students whose cohort matches the
-    # argument supplied. Remember we converted the month from a string to a
-    # symbol.
-  students = students.select { |student| student[:cohort] == cohort}
-  end
-  students.sort_by {|cohort| cohort[:cohort]}.each do |student|
-    puts "  #{student[:name]}".ljust(40) + "(#{student[:cohort]} cohort)".ljust(@line_width)
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  puts "-------------".center(@line_width)
-  puts ("Overall, we have #{students.count} great " + (students.count > 1 ? "students" : "student")).center(@line_width)
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
-# nothing happens until we call the methods
+#nothing happens until we call the methods
 print_header
-print(students) # optional argument to sepecify cohort month.
+print(students)
 print_footer(students)
