dfaaa196da2dbef8cc522142d480f9c4e0605c9b
BDCraven <bencraven@hotmail.com>
2017-06-20 11:29:57 +0100

    Modify input_students to ask for name and cohort.

--- directory.rb
+++ directory.rb
@@ -1,17 +1,33 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names and cohorts of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
+  cohort_months = ["January", "February", "March", "April",
+             "May", "June", "July", "August",
+             "September", "October", "November", "December"]
   # get the first name
-  name = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while true do
+    puts "Please enter the student's full name"
+    name = gets.chomp
+    break if name.empty?
+
+    puts "Please enter the month of the student's cohort or leave blank if you are not sure"
+    cohort = gets.chomp.capitalize
+
+    while !cohort_months.include?(cohort) && cohort != ""
+      puts "That is not a recognised month please enter a month or leave blank if you are not sure"
+      cohort = gets.chomp.capitalize
+    end
+
+    if cohort == ""
+      cohort = "TBC"
+    end
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
   end
   # return the array of students
   students
